467e55e599454f8f7c45748bf5382d2381ed6959
unknown <1124892302@qq.com>
2021-09-19 13:36:36 +0800

    关键字提取v2.1：实现了level2 switch和case的统计

--- commit.cpp
+++ commit.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 #include <fstream> 
 using namespace std;
-	int cnt = 0;
-	
+	int cnt1 = 0;
+	string match[1000];
 	string key[40]={"auto","break","case","char","const","continue","default","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"};
 
 int BruteForce(string t, string p){
@@ -18,7 +18,7 @@ int BruteForce(string t, string p){
 			}
 		}
 		if (j == lenP){
-			cnt++;
+			cnt1++;
 			return 1;
 		}
 	}
@@ -38,17 +38,59 @@ void TextFileReadByLine(char** argv)
 		
 		for(int i = 0;i < 31; i++){
 		
-			BruteForce(buffer, key[i]);
+			if(BruteForce(buffer, key[i]))
+			match[cnt1]=key[i];
 		}
 		//cout<<buffer<<endl;
 	}
 	fFile.close();
 }
+void level_2(string str[]);
 int main(int argc, char **argv){
 	
 	TextFileReadByLine(argv);
+	
+		char level=*argv[2];
+		
+		switch (level){
+			
+		case '1':break;
+		
+		case '2':{
+		level_2(match);break;
+		}
 
-	cout << "total num:" << cnt << endl;
+	cout << "total num:" << cnt1 << endl;
 
 	return 0;
 	}
+}
+void level_2(string str[]){
+	
+	int temp1 = 0,temp2[100] = {0};
+	int tmp = 0;
+	
+	for(int i = 0 ; i < cnt1 ; i++){
+		
+		if (str[i] == "switch")
+		
+			temp1++;
+		
+		else if(str[i] == "case")
+		
+			temp2[tmp]++;
+		
+		else if(str[i] == "default")
+		
+			tmp++;
+	}
+	cout<<"switch num:"<<temp1<<endl;
+	
+	cout<<"case num:";
+	
+	for(int i = 0;i < tmp ; i++)
+	
+	cout<<temp2[i]<<" ";
+	
+	cout<<endl;
+}
